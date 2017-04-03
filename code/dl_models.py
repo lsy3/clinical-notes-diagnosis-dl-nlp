5bc9f0e2b4c7833a4409ddab36cab78441a71c56
Vic Huang <vichuang@gatech.edu>
2017-04-03 21:20:37 +0000

    add glove lstm train

--- code/dl_models.py
+++ code/dl_models.py
@@ -1,8 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Embedding, BatchNormalization, Activation
-from keras.layers import LSTM
-
-
+from keras.layers import Dense, Dropout, Activation, Embedding, BatchNormalization, LSTM
 
 def nn_model_1(feature_size):
     print('Build model...')
@@ -39,7 +36,28 @@ def nn_model_2(feature_size):
     return model
 
 
+def lstm_model_1(vocabulary_size, embedding_matrix, max_sequence_length, category_number, input_shape):
+    # LSTM with embedding trainable
+    embedding_dim = embedding_matrix.shape[1]
+
+    print('Build model...')
+    model = Sequential()
+    model.add(Embedding(vocabulary_size,
+                        embedding_dim,
+                        weights=[embedding_matrix],
+                        input_length=max_sequence_length,
+                        trainable=True,
+                        input_shape=input_shape))
+    model.add(LSTM(256))
+    model.add(Dropout(0.5))
+    model.add(BatchNormalization())
+    model.add(Dense(category_number, activation='sigmoid'))
+
+    model.compile(loss='mse', optimizer='rmsprop', metrics=['mse'])
+    model.summary()
+
+
 def get_function_dict():
-    funcs = {'nn_model_1': nn_model_1, 'nn_model_2': nn_model_2}
+    funcs = {'nn_model_1': nn_model_1, 'nn_model_2': nn_model_2, 'lstm_model_1': lstm_model_1}
     return funcs
 
